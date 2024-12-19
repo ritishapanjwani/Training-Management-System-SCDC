14d4864e402d7f2f5be7d4c7afe696d81fff2e70
Sanjay R <sanjay.r@solugenix.com>
2024-12-19 21:15:35 +0530

    side bar completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { DefaultComponent } from './admin/default/default.component';
+import { SidebarComponent } from './admin/shared/sidebar/sidebar.component';
+
+
+
+
+//creating a constant array of all the modules
+const uxModules = [
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatToolbarModule,
+  RouterOutlet
+];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule, 
+    uxModules
   ],
   providers: [],
   bootstrap: [AppComponent]
