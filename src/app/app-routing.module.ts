d799660c6d55a3e33b484b6cd53b20895cc3135f
Sanjay R <sanjay.r@solugenix.com>
2024-12-19 22:56:08 +0530

    sidebar updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './admin/default/default.component';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
 
 const routes: Routes = [
   {path: '', component:DefaultComponent}, 
+  {path:'sidebar', component:SidebarComponent}
 ];
 
 @NgModule({
